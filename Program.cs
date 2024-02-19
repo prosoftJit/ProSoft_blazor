0233e6275e3afc4f97572c6f2353d1f1d304063a
prosoftJit <31407366+prosoftJit@users.noreply.github.com>
2024-02-19 18:00:04 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using ProSoft.Data;
+using ProSoft.Services;
+using Radzen;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddBlazorBootstrap();
+
+builder.Services.AddRadzenComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
